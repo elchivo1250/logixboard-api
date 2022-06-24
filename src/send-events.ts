a6d7ab32ef0e499874a7330e68d09360ba00af38
Steve Preston <steve.preston.80@gmail.com>
2022-06-24 06:01:36 +0000

    Added send-events script and messages

---
+++ src/send-events.ts
@@ -0,0 +1,27 @@
+import { messages } from './messages'
+import axios from 'axios'
+
+async function main() {
+    for (let i = 0; i < messages.length; i++) {
+        const message = messages[i]
+        let endpoint = 'shipment'
+        if (message.type === 'ORGANIZATION') {
+            endpoint = 'organization'
+        }
+
+        try {
+            await axios.post(`http://localhost:3000/${endpoint}`, message)
+        } catch (error) {
+            console.error(error.code)
+        }
+    }
+
+    try {
+        const res = await axios.get(`http://localhost:3000/shipments/aggregate-weight/GRAMS`);
+        console.log(res.data);
+    } catch (error) {
+        console.error(error.code)
+    }
+}
+
+main()
